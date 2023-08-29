6cadbfbba45f4e25dc88e67323bc86ec134fe705
BoyoungYun <index_1234@naver.com>
2023-08-30 00:22:57 +0900

    feat: Set router in detail page

--- src/App.js
+++ src/App.js
@@ -12,12 +12,13 @@ export default class App extends Component {
   mounted() {
     const $main = this.$target.querySelector("main");
     const pages = createPages($main);
+    const id = [...Array(10).keys()].map(i => i); // 추후 데이터 불러온 뒤 데이터 id 배열로 교체 필요(임시로 0~9까지 넣어 두었습니다.)
 
     const router = new Router($main);
     router.addRoute("#/", pages.landing);
     router.addRoute("#/category", pages.category);
     router.addRoute("#/search", pages.search);
-    router.addRoute("#/detail", pages.detail);
+    id.forEach(item => router.addRoute(`#/detail/${item}`, pages.detail));
     router.start();
   }
 }
