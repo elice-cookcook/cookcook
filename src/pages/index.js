94e69b0deed68a9c0ee06140e7314a3f2afc3e4c
spritte <elicespritte@gmail.com>
2023-08-31 00:42:58 +0900

    change to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,17 @@ import LandingPage from './LandingPage.js';
 import CategoryPage from './CategoryPage.js';
 import SearchPage from './SearchPage.js';
 import DetailPage from './DetailPage.js';
-import SplashPage from './SplashPage.js';
 
 export default (main) => {
   const landing = () => new LandingPage(main);
   const category = () => new CategoryPage(main);
   const search = () => new SearchPage(main);
   const detail = () => new DetailPage(main);
-  const splash = () => new SplashPage(main);
 
   return {
     landing,
     category,
     search,
     detail,
-    splash,
   };
 };
