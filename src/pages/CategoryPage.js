3a292bcae2c3d01871347be1a0faf744d08206c3
Susan-Lee-01 <sunwoo40101@gmail.com>
2023-08-30 11:55:35 +0900

    Recommit

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,19 +1,16 @@
 import Component from '../core/Component.js';
-import Header from '../components/Header.js';
-import Navigator from '../components/Navigator.js';
+import Filter from '../components/filter.js';
 
 export default class CategoryPage extends Component {
   template() {
-    return `
-        <h1>Main Page</h1>
-        <div id="header"></div>
-        <div id="nav"></div>
+    return ` 
+      <h1>Category Page</h1>
+      <div id="filter"></div>      
     `;
   }
+
   mounted() {
-    const $header = this.$target.querySelector("#header");
-    const $nav = this.$target.querySelector("#nav");
-    new Header($header);
-    new Navigator($nav);
+    const $filter = this.$target.querySelector("#filter");
+    new Filter($filter);
   }
 }
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,9 +1,16 @@
 import Component from '../core/Component.js';
+import Filter from '../components/filter.js';
 
 export default class SearchPage extends Component {
   template() {
     return `
-        <h1>Search Page</h1>
+      <h1>Search Page</h1>
+      <div id="filter"></div>      
     `;
+  }
+
+  mounted() {
+    const $filter = this.$target.querySelector("#filter");
+    new Filter($filter);
   }
 }
