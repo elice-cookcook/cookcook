56c82b8f3a6c68519c0ec31cd6a55f0c3fc477e9
Susan-Lee-01 <sunwoo40101@gmail.com>
2023-08-30 12:23:38 +0900

    change staged search page code

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -4,7 +4,6 @@ import Filter from '../components/filter.js';
 export default class SearchPage extends Component {
   template() {
     return `
-      <h1>Search Page</h1>
       <div id="filter"></div>      
     `;
   }
