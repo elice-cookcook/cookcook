9c1e04dfe8ca09804df27df78058ac2e59d38b68
Susan-Lee-01 <sunwoo40101@gmail.com>
2023-08-30 12:39:01 +0900

    revert to original

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -4,6 +4,7 @@ import Filter from '../components/filter.js';
 export default class SearchPage extends Component {
   template() {
     return `
+      <h1>Search Page</h1>
       <div id="filter"></div>      
     `;
   }
