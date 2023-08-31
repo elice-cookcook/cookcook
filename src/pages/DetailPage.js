7d7df3d516d77fc75302ede418206a2e7a89bd08
SunWoo Lee <109502469+Susan-Lee-01@users.noreply.github.com>
2023-08-31 22:08:44 +0900

    Update DetailPage.js

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,9 +1,31 @@
 import Component from '../core/Component.js';
+import Header from '../components/Header.js';
+import Navigator from '../components/Navigator.js';
 
 export default class DetailPage extends Component {
   template() {
     return `
+      <style>
+      .DetailPage {
+        width: 480px;
+        left: 50%;
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+      </style>
+      <div class="DetailPage">
+        <div id="header"></div>
+        <div id="nav"></div>
+      </div> 
       <h1>Detail Page</h1>
     `;
+  }
+  mounted() {
+    const $header = this.$target.querySelector("#header");
+    const $nav = this.$target.querySelector("#nav");
+    new Header($header, "detail");
+    new Navigator($nav);
   }
 }
