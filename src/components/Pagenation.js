09282d6fdf451ee6d1eb1050cfbf59da2db69378
spritte <elicespritte@gmail.com>
2023-09-01 15:54:06 +0900

    change class to bootstrap

--- src/components/Pagenation.js
+++ src/components/Pagenation.js
@@ -4,10 +4,7 @@ export default class Pagenation extends Component {
     template() {
       return `
       <style>
-        #pagination li{
-          margin-left: 16px;
-        }
-        #pagination .box a{
+        #pagination .page-item a{
           border: none;
           border-radius: 4px;
           background-color:#f4f4f4;
@@ -19,18 +16,18 @@ export default class Pagenation extends Component {
         }
       </style>
       <nav id="pagination">
-      <ul class="pagination space">
-        <li class="page-item box">
+      <ul class="pagination">
+        <li class="page-item mx-2">
           <a class="page-link" href="#" aria-label="Previous">
             <span aria-hidden="true"><</span>
           </a>
         </li>
-        <li class="page-item box active"><a class="page-link" href="#">1</a></li>
-        <li class="page-item box"><a class="page-link" href="#">2</a></li>
-        <li class="page-item box"><a class="page-link" href="#">3</a></li>
-        <li class="page-item box"><a class="page-link" href="#">4</a></li>
-        <li class="page-item box"><a class="page-link" href="#">5</a></li>
-        <li class="page-item box">
+        <li class="page-item mx-2 active"><a class="page-link" href="#">1</a></li>
+        <li class="page-item mx-2"><a class="page-link" href="#">2</a></li>
+        <li class="page-item mx-2"><a class="page-link" href="#">3</a></li>
+        <li class="page-item mx-2"><a class="page-link" href="#">4</a></li>
+        <li class="page-item mx-2"><a class="page-link" href="#">5</a></li>
+        <li class="page-item mx-2">
           <a class="page-link" href="#" aria-label="Next">
             <span aria-hidden="true">></span>
           </a>
