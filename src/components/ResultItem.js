170f3f1192e23ad85b81233ea759b380a636d6d6
cjy921004 <ej921004@naver.com>
2023-09-08 00:09:02 +0900

    Design : Fix recipt img size

--- src/components/ResultItem.js
+++ src/components/ResultItem.js
@@ -7,33 +7,48 @@ export default class ResultItem extends Component {
     let imgUrl = this.$props.ATT_FILE_NO_MAIN;
     let ingredients = this.$props.RCP_PARTS_DTLS;
     let hash_tag = this.$props.HASH_TAG;
+    let id = this.$props.RCP_SEQ;
 
-    return /*html*/ `
-    <style>
-      .ResultItem{
+    return /*html*/ `<style>
+    .ResultItem {
         width: 99%;
-        height:130px;
-        display:flex;
-        background-color:#ffebcb;
-        border-radius:5px;
-        align-items:center;
-        text-align:center;
-      }
-      .ResultItem > img{
+        height: 130px;
+        display: flex;
+        background-color: #ffebcb;
+        border-radius: 5px;
+        align-items: center;
+        text-align: center;
+    }
+
+    .ResultItem_imgSection{
+        flex:1;
+    }
+
+    .ResultItem_img {
         width:120px;
         height:120px;
-        object-fit:cover;
-      }
-      .ResultItem_info_top{
-        display:flex;
-        justify-content:space-around;
-        font-size:large;
-      }
-      .ResultItem_name{
-        font-weight:bold;
-      }
-      .ResultItem_ingredients{
-        font-size:small;
+    }
+
+    .ResultItem_info {
+        flex:3;
+    }
+
+    .ResultItem_name {
+        font-weight: bold;
+        font-size: 16px;
+        word-break : keep-all;
+    }
+
+    .ResultItem_calorie{
+      font-size:12px;
+      color:gray;
+      justify-content:center;
+      align-items:center;
+      align-content:center;
+    }
+
+    .ResultItem_ingredients {
+        font-size: 12px;
         overflow: hidden;
         white-space: normal;
         text-overflow: ellipsis;
@@ -41,24 +56,25 @@ export default class ResultItem extends Component {
         -webkit-line-clamp: 2;
         -webkit-box-orient: vertical;
         word-break: keep-all;
-      }
-      .ResultItem_hashtag{
-        text-align:center;
-      }
-    </style>
-        <div class="ResultItem container mx-auto my-3 p-2 w-95">
-        <img src="${imgUrl}" class="ResultItem_img"/>
-        <div class="ResultItem_info my-1 mx-auto">
-        <div class="ResultItem_info_top">
-        <span class="ResultItem_name">${name}</span>
-        <span class="ResultItem_calorie">${calorie}kcal</span>
-        </div>
+    }
+
+    .ResultItem_hashtag {
+        font-size: 13px;
+        font-weight:bold;
+    }
+
+</style>
+<div class="ResultItem container mx-auto my-3 p-2 w-95">
+    <div class="ResultItem_imgSection">
+        <img src="${imgUrl}" class="ResultItem_img" /></div>
+    <div class="ResultItem_info my-1 mx-auto">
+        <p class="ResultItem_name my-1">${name}  <span class="ResultItem_calorie">${calorie}kcal</span></p>
         <p class="ResultItem_ingredients">재료 : ${ingredients}</p>
         <span class="ResultItem_hashtag orange">${
           hash_tag.length > 0 ? "# " + hash_tag : ""
         }<span>
-        </div>
-        </div>
+    </div>
+</div>
     `;
   }
 }
