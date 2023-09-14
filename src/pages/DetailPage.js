a3562cd5c7bdfb9d59287af5fb20198347f6960f
spritte <elicespritte@gmail.com>
2023-09-15 00:53:54 +0900

    first

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -120,54 +120,54 @@ export default class DetailPage extends Component {
     <p class="DetailPage_INFO_ENG orange"></p>
     <img class="DetailPage_img w-50" src="https://placehold.co/240x240?text=image" />
     <div>
-      <div class="DetailPage_tip title my-3">
-          <hr width="20%" />
-          <div class="titleText orange">저감조리법 Tip</div>
-          <hr width="20%" />
-      </div>
-      <div class="DetailPage_RCP_NA_TIP content text-center p-3 my-2"></div>
+        <div class="DetailPage_tip title my-3">
+            <hr width="20%" />
+            <div class="titleText orange">저감조리법 Tip</div>
+            <hr width="20%" />
+        </div>
+        <div class="DetailPage_RCP_NA_TIP content text-center p-3 my-2"></div>
     </div>
     <div>
-      <div class="DetailPage_ingredients title my-3">
-          <hr width="20%" />
-          <div class="titleText orange">준비물</div>
-          <hr width="20%" />
-      </div>
-      <div class="DetailPage_RCP_PARTS_DTLS content text-center p-3 my-2"></div>
+        <div class="DetailPage_ingredients title my-3">
+            <hr width="20%" />
+            <div class="titleText orange">준비물</div>
+            <hr width="20%" />
+        </div>
+        <div class="DetailPage_RCP_PARTS_DTLS content text-center p-3 my-2"></div>
     </div>
     <div>
-      <div class="DetailPage_recepi title mt-4">
-          <hr width="20%" />
-          <div class="titleText orange">레시피</div>
-          <hr width="20%" />
-      </div>
+        <div class="DetailPage_recepi title mt-4">
+            <hr width="20%" />
+            <div class="titleText orange">레시피</div>
+            <hr width="20%" />
+        </div>
 
-      <div class="DetailPage_menu">
-      <button id="hideBtn" class="buttonBefore">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
-          <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-          <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
-        </svg>
-      </button>
-      <button id="printBtn" class="buttonBefore">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
-          <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
-          <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z"/>
-        </svg>
-      </button>
-      </div>
-
-      <div id="recipe" class="DeatailPage_recipe p-3"></div>
+        <div class="DetailPage_menu">
+        <button id="hideBtn" class="buttonBefore">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
+                <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
+            </svg>
+        </button>
+        <button id="printBtn" class="buttonBefore">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
+                <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
+                <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z"/>
+            </svg>
+        </button>
     </div>
-      <hr width="90%"/>
-  <section class="DetailPage_bottom">
-      <div class="DetailPage_">
-          <img class="DetailPage_logo" src="./img/cookcooklogo.png" />이 알려준 레시피가 마음에 들었다면?
-      </div>
-      <div class="DetailPage_shareElemSection mt-2 mb-4">
-      </div>
-  </section>
-  <div id="footer"></div>
+
+    <div id="recipe" class="DeatailPage_recipe p-3"></div>
+    </div>
+        <hr width="90%"/>
+    <section class="DetailPage_bottom">
+        <div class="DetailPage_">
+            <img class="DetailPage_logo" src="./img/cookcooklogo.png" />이 알려준 레시피가 마음에 들었다면?
+        </div>
+        <div class="DetailPage_shareElemSection mt-2 mb-4">
+        </div>
+    </section>
+    <div id="footer"></div>
 </div></div>
     `;
   }
