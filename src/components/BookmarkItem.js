71868a6395fa736b95f9f1777b3b26aa8c5cb834
cjy921004 <ej921004@naver.com>
2023-09-14 10:08:00 +0900

    Feat : add BookmarkItem in DetailPage

--- src/components/BookmarkItem.js
+++ src/components/BookmarkItem.js
@@ -3,56 +3,73 @@ import Component from "../core/Component.js";
 export default class BookmarkItem extends Component {
   template() {
     let name = this.$props.RCP_NM;
-    let calorie = this.$props.INFO_ENG;
     let imgUrl = this.$props.ATT_FILE_NO_MAIN;
-    let ingredients = this.$props.RCP_PARTS_DTLS;
     let hash_tag = this.$props.HASH_TAG;
+    let category = this.$props.RCP_PAT2;
+    let calorie = this.$props.INFO_ENG;
 
-    return /*html*/ `<style>
+    return /*html*/ `
+    <style>
     .BookmarkItem {
-        width: 99%;
-        height: 130px;
+        height: 100%;
         display: flex;
-        background-color: #ffebcb;
-        border-radius: 5px;
-        align-items: center;
-        text-align: center;
-        cursor:pointer;
+        flex-direction: column;
+        justify-content:space-around;
+        cursor: pointer;
     }
 
-    .BookmarkItem_imgSection{
-        flex:1;
+    .BookmarkItem_delete{
+      z-index:1;
+      width:100%; 
+      height:10px;
+      color:gray;
+  text-align: right;
     }
 
-    .BookmarkItem_img {
-        width:120px;
-        height:120px;
-    }
-
-    .BookmarkItem_info {
-        flex:3;
-    }
+    .BookmarkItem_imgDiv {
+    width: 100%;
+    height: 130px;
+    border-radius: 5px;
+    overflow:hidden;
+}
+.BookmarkItem_img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
 
     .BookmarkItem_name {
         font-weight: bold;
-        font-size: 16px;
-        word-break : keep-all;
+        font-size: 12px;
+        overflow: hidden;
+        text-align: left;
+        word-wrap: break-word;
+        display: -webkit-box;
+        -webkit-line-clamp: 1 ;
+        -webkit-box-orient: vertical;
     }
 
     .BookmarkItem_calorie{
-      font-size:12px;
-      color:gray;
-      justify-content:center;
-      align-items:center;
-      align-content:center;
+        font-size: 11px;
+        color:gray;
+    }
+
+    .BookmarkItem_hashtag{
+        font-size: 10px;
     }
 
 </style>
-<div class="BookmarkItem container mx-auto my-3 p-2 w-95">
-    <div class="BookmarkItem_imgSection">
-        <img src="${imgUrl}" class="BookmarkItem_img" /></div>
-    <div class="BookmarkItem_info my-1 mx-auto">
-        <p class="BookmarkItem_name my-1">${name}  <span class="BookmarkItem_calorie">${calorie}kcal</span></p>
+
+<div class="BookmarkItem p-1">
+        <span class="BookmarkItem_delete">❌</span>
+    <div class="BookmarkItem_imgDiv">
+      <img src="${imgUrl}" class="BookmarkItem_img" />  </div>
+    <div class="BookmarkItem_nameDiv">
+        <span class="BookmarkItem_name mt-1">${name}</span>
+        <span class="BookmarkItem_calorie">${calorie}kcal</span>
+            <span class="BookmarkItem_hashtag orange">${
+              hash_tag.length > 0 ? "# " + hash_tag : ""
+            }<span>
     </div>
 </div>
     `;
--- src/pages/BookmarkPage.js
+++ src/pages/BookmarkPage.js
@@ -1,79 +1,113 @@
 import Component from "../core/Component.js";
 import Header from "../components/Header.js";
-import Navigator from "../components/Navigator.js";
 import Footer from "../components/Footer.js";
-import Pagination from "../components/Pagination.js";
+import BookmarkItem from "../components/BookmarkItem.js";
 
 export default class BookmarkPage extends Component {
   setup() {
     this.$state = [
       {
         RCP_NM: "크림소스치킨롤",
-        INFO_ENG: "234.12",
         ATT_FILE_NO_MAIN:
           "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00670_2.png",
-        RCP_PARTS_DTLS:
-          "닭고기(가슴살, 150g), 새우(대하, 3마리), 베이컨(20g),\n마늘(20g), 바질…새송이버섯(1개),\n치즈(1장), 버터(10g), 소금(0.2g), 후춧가루(0.01g)",
+        INFO_ENG: "234.12",
         HASH_TAG: "닭가슴살",
+        RCP_PAT2: "국",
       },
       {
         RCP_NM: "치킨 쇠고기 땅콩소스 꼬치",
-        INFO_ENG: "148",
         ATT_FILE_NO_MAIN:
           "http://www.foodsafetykorea.go.kr/uploadimg/20141117/20141117053805_1416213485286.jpg",
-        RCP_PARTS_DTLS:
-          "닭가슴살 30g, 쇠고기 등심 30g, 간장 3g, 카레가루 2g, 땅콩버터 3g, 참기름 0.5g, 올리브오일 2g, 설탕 1.5g, 생강다진것 1g",
+        INFO_ENG: "234.12",
         HASH_TAG: "가슴살",
+        RCP_PAT2: "반찬",
       },
       {
         RCP_NM: "치킨완자스프",
-        INFO_ENG: "236.7",
         ATT_FILE_NO_MAIN:
           "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00465_2.png",
-        RCP_PARTS_DTLS:
-          "버터(20g), 밀가루(20g), 육수(100g), 생크림(20g)\n브로컬리(30g), 우유(100g), 마늘(10g), 양파(20g)\n소금(0.3g), 후춧가루(0.05g), 참기름(5g)",
+        INFO_ENG: "234.12",
         HASH_TAG: "",
+        RCP_PAT2: "반찬",
       },
       {
         RCP_NM: "크림소스치킨롤",
-        INFO_ENG: "234.12",
         ATT_FILE_NO_MAIN:
           "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00670_2.png",
-        RCP_PARTS_DTLS:
-          "닭고기(가슴살, 150g), 새우(대하, 3마리), 베이컨(20g),\n마늘(20g), 바질…새송이버섯(1개),\n치즈(1장), 버터(10g), 소금(0.2g), 후춧가루(0.01g)",
+        INFO_ENG: "234.12",
+        HASH_TAG: "",
+        RCP_PAT2: "반찬",
+      },
+      {
+        RCP_NM: "크림소스치킨롤",
+        ATT_FILE_NO_MAIN:
+          "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00670_2.png",
+        INFO_ENG: "234.12",
+        HASH_TAG: "",
+        RCP_PAT2: "밥",
+      },
+      {
+        RCP_NM: "크림소스치킨롤",
+        ATT_FILE_NO_MAIN:
+          "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00670_2.png",
+        INFO_ENG: "234.12",
+        HASH_TAG: "",
+        RCP_PAT2: "반찬",
         HASH_TAG: "닭가슴살",
+      },
+      {
+        RCP_NM: "크림소스치킨롤",
+        ATT_FILE_NO_MAIN:
+          "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00670_2.png",
+        INFO_ENG: "234.12",
+        HASH_TAG: "",
+        RCP_PAT2: "반찬",
+        HASH_TAG: "닭가슴살",
+      },
+      {
+        RCP_NM: "크림소스치킨롤",
+        ATT_FILE_NO_MAIN:
+          "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00670_2.png",
+        INFO_ENG: "234.12",
+        HASH_TAG: "",
+        RCP_PAT2: "반찬",
       },
     ];
   }
   template() {
-    return /*html*/ `
-    <style>
+    return /*html*/ `<style>
     .BookmarkPage {
         margin: 0 auto;
         border: 1px solid #eaeaea;
-        width: 480px;           
-        left: 50%;
+        width: 480px;
         flex-direction: column;
         align-items: center;
-        word-break : keep-all;
-        display:flex;
+        word-break: keep-all;
+        display: flex;
     }
+
     .orange {
         color: orange;
-              }
+    }
 
-       .BookmarkPage_top {
-        width:100%;
-       } 
+    .BookmarkPage_top {
+        width: 100%;
+    }
 
+    .BookmarkContainer {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;     
+        gap:10px;
+    }
 </style>
 
 <div class="BookmarkPage px-3">
     <div id="header"></div>
 
-        <div id="nav"></div>
+    <div id="nav"></div>
     <div class="BookmarkPage_top mt-2">
-    <span class="orange">🧡 찜 목록 > ${this.$state.length}개의 레시피가 있어요.</span></div>
+        <span class="orange">찜 목록 > ${this.$state.length}개의 레시피가 있어요.</span></div>
+    <div class="BookmarkContainer p-1"></div>
     <div id="paginationContainer"></div>
     <div id="footer"></div>
 </div>
@@ -90,8 +124,16 @@ export default class BookmarkPage extends Component {
     const $nav = this.$target.querySelector("#nav");
     // new Navigator($nav);
 
+    const bookmarkContainer = this.$target.querySelector(".BookmarkContainer");
+    this.$state.forEach((item) => {
+      const div = document.createElement("div");
+      new BookmarkItem(div, item);
+      bookmarkContainer.append(div);
+    });
+    const plus = document.createElement("div");
+    bookmarkContainer.append(plus);
     const $pagination = this.$target.querySelector("#paginationContainer");
-    new Pagination($pagination, this.$state);
+    // new Pagination($pagination, this.$state);
     const $footer = this.$target.querySelector("#footer");
     new Footer($footer);
   }
