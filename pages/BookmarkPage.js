48336e51dd9992377a8eb1e7b3b01f3a464140ef
cjy921004 <ej921004@naver.com>
2023-09-19 00:33:09 +0900

    Feat : load BookmarkList after remove Bookmark

--- pages/BookmarkPage.js
+++ pages/BookmarkPage.js
@@ -2,18 +2,26 @@ import Component from "../core/Component.js";
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
 import BookmarkItem from "../components/BookmarkItem.js";
+import {
+    getLocalStorageData,
+    setLocalStorageData,
+} from "../utils/useLocalStorage.js";
 
 export default class BookmarkPage extends Component {
   setup() {
-    const bookmark =
-      localStorage.getItem("bookmark") === null
-        ? []
-        : JSON.parse(localStorage.getItem("bookmark"));
-    this.setState({
-      toggleCheck: false,
-      data: bookmark,
-    });
-  }
+        const bookmark =
+            getLocalStorageData("bookmark") === null ?
+            [] :
+            getLocalStorageData("bookmark");
+
+        this.setState({
+            toggleCheck: false,
+            data: bookmark,
+        });
+
+        setLocalStorageData("deleteTargets", []);
+        this.targetNum = 0;
+    }
   template() {
     return /*html*/ `<style>
     .BookmarkPage {
@@ -57,11 +65,10 @@ export default class BookmarkPage extends Component {
 </style>
 <div class="BookmarkPage px-3">
     <div id="header"></div>
-    <div id="nav"></div>
     <div class="BookmarkPage_top">
-        <span class="orange">찜 목록 &nbsp;>&nbsp; ${this.$state.data.length}개의 레시피가 있어요.</span>
+        <span class="orange">북마크 목록 &nbsp;>&nbsp; ${this.$state.data.length}개의 레시피가 있어요.</span>
         <div>
-        <span class="BookmarkPage_checkedNum orange mx-1">2개 선택</span>
+        <span class="BookmarkPage_checkedNum orange mx-1">${this.targetNum}개 선택</span>
         <span class="BookmarkPage_deleteBtn btn btn-outline-warning">삭제</span>
         <span class="BookmarkPage_checkeBtn btn btn-outline-warning" data-bs-toggle="button">선택</span>
         </div>
@@ -70,53 +77,72 @@ export default class BookmarkPage extends Component {
 <div id="footer"></div>
 </div>
     `;
-  }
-  async mounted() {
-    const $header = this.$target.querySelector("#header");
-    new Header($header, {
-      page: "bookmark",
-      category: "",
-      keyword: history.state ? history.state.keyword : "",
-    });
-
-    const checkdNum = this.$target.querySelector(".BookmarkPage_checkedNum");
-    const toggleBtn = this.$target.querySelector(".BookmarkPage_checkeBtn");
-    const deleteBtn = this.$target.querySelector(".BookmarkPage_deleteBtn");
-
-    if (this.$state.toggleCheck) {
-      toggleBtn.classList.add("active");
-      toggleBtn.innerHTML = "취소";
-      deleteBtn.style.display = "";
-      checkdNum.style.display = "";
-    } else {
-      toggleBtn.classList.remove("active");
-      toggleBtn.innerHTML = "선택";
-      deleteBtn.style.display = "none";
-      checkdNum.style.display = "none";
     }
+    setDeleteTargetNum = (num) => {
+        this.$target.querySelector(".BookmarkPage_checkedNum") = innerHTML(`${num}개 선택`);
+    }
+    async mounted() {
+        const $header = this.$target.querySelector("#header");
+        new Header($header, {
+            page: "bookmark",
+            category: "",
+            keyword: history.state ? history.state.keyword : "",
+        });
+        const checkdNum = this.$target.querySelector(".BookmarkPage_checkedNum");
+        const toggleBtn = this.$target.querySelector(".BookmarkPage_checkeBtn");
+        const deleteBtn = this.$target.querySelector(".BookmarkPage_deleteBtn");
 
-    deleteBtn.addEventListener("click", () => {
-      if (confirm(`2개의 레시피가 삭제됩니다.`)) this.setup();
-    });
+        if (this.$state.toggleCheck) {
+            toggleBtn.classList.add("active");
+            toggleBtn.innerHTML = "취소";
+            deleteBtn.style.display = "";
+            checkdNum.style.display = "";
+        } else {
+            toggleBtn.classList.remove("active");
+            toggleBtn.innerHTML = "선택";
+            deleteBtn.style.display = "none";
+            checkdNum.style.display = "none";
+        }
 
-    toggleBtn.addEventListener(
-      "click",
-      function () {
-        let isSelected = this.$state.toggleCheck;
-        this.setState({ toggleCheck: !isSelected, data: this.$state.data });
-      }.bind(this)
-    );
+        deleteBtn.addEventListener("click", () => {
+            if (confirm(`${this.targetNum}개의 레시피가 삭제됩니다.`)) {
+                const deleteTargets = getLocalStorageData("deleteTargets")
+                let bookmark = getLocalStorageData("bookmark")
+                for (const id of deleteTargets)
+                    bookmark = bookmark.filter(item => item.RCP_SEQ !== id)
+                setLocalStorageData("bookmark", bookmark)
+                this.setup()
+            }
+        });
 
-    const bookmarkContainer = this.$target.querySelector(".BookmarkContainer");
-    this.$state.data.forEach((item) => {
-      const div = document.createElement("div");
-      new BookmarkItem(div, item, this.$state.toggleCheck);
-      bookmarkContainer.append(div);
-    });
-    const plus = document.createElement("div");
-    bookmarkContainer.append(plus);
+        toggleBtn.addEventListener(
+            "click",
+            function() {
+                let isSelected = this.$state.toggleCheck;
+                this.setState({
+                    toggleCheck: !isSelected,
+                    data: this.$state.data
+                });
+            }.bind(this)
+        );
 
-    const $footer = this.$target.querySelector("#footer");
-    new Footer($footer);
-  }
+        const bookmarkContainer = this.$target.querySelector(".BookmarkContainer");
+        this.$state.data.forEach((item) => {
+            const div = document.createElement("div");
+            new BookmarkItem(div, item, this.$state.toggleCheck);
+            bookmarkContainer.append(div);
+        });
+
+        const checkboxes = document.querySelectorAll(".BookmarkItem input");
+
+        checkboxes.forEach((checkbox) => {
+            checkbox.addEventListener("click", () => {
+                checkbox.checked ? this.targetNum++ : this.targetNum--;
+                this.$target.querySelector(".BookmarkPage_checkedNum").innerHTML = `${this.targetNum}개 선택`;
+            });
+        });
+
+        const $footer = this.$target.querySelector("#footer");
+        new Footer($footer);
+    }
 }
