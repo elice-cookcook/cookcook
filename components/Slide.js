1965858380049af5329177a2dd5e7d7cb69a5b8f
cjy921004 <ej921004@naver.com>
2023-09-20 19:46:19 +0900

    Design : split style code at Slide

--- components/Slide.js
+++ components/Slide.js
@@ -1,40 +1,6 @@
 import Component from "../core/Component.js";
 
 export default class Slide extends Component {
-  template() {
-    return /*html*/ `
-      <style>
-        .slide {
-          flex: 1;
-          display: flex;
-          align-items: center;
-          margin: 0 10px;
-          width: 120px;
-          height: 120px;
-          cursor: pointer;
-          position: relative;
-        }
-        .slide img {
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-        }
-        .image-name {
-          position: absolute;
-          padding: 3px 0; 
-          bottom: 5px;
-          width: 120px;
-          font-size: 14px;
-          font-weight: bold;
-          background-color: rgba(227, 227, 227, 0.632);
-          line-height: 1.2em;
-          overflow:hidden;
-          text-overflow:ellipsis;
-          white-space:nowrap;
-        }
-      </style>
-       `;
-  }
   setEvent() {
     this.addEvent("click", ".slide", (e) => {
       if (selectedItem) {
